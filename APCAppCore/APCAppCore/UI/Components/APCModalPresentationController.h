9e84967564b998888d017afc3bac269a880e0cd3
Everest Liu <everest@hellothread.com>
2016-02-16 17:50:41 -0800

    PAN-68 - Add PresentationController

---
+++ APCAppCore/APCAppCore/UI/Components/APCModalPresentationController.h
@@ -0,0 +1,12 @@
+//
+//  APCModalPresentationController.h
+//  APCAppCore
+//
+//  Created by Everest Liu on 2/16/16.
+//  Copyright © 2016 Thread, Inc. All rights reserved.
+//
+
+@import UIKit;
+
+@interface APCModalPresentationController : UIPresentationController <UIViewControllerTransitioningDelegate>
+@end
